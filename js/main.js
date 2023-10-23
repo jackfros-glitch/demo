60fb5b16ab190590a50193c13094ccea12a40ade
jackfros <hirekaanm3@gmail.com>
2023-10-23 06:55:54 +0100

    feat:animation, add the typed.js library to aniamate the intro text.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+AOS.init();
+// Setup and start animation!
+var typed = new Typed('.typed-text', {
+strings: ['I am Manasseh <span class="name">Hirekaan</span>', "I'm a Software <span class='name'> Developer</span>", "I'm a Frontend <span class='name'> Developer</span>", "I'm a Backend <span class='name'>Developer</sapn>"],
+typeSpeed: 50,
+loop:true
+});
+
+
 const collapsibles = document.querySelectorAll('.collapsible');
 collapsibles.forEach((item)=>{
     item.addEventListener('click', function (){
